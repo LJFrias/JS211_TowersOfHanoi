b3f7285cf9d0143a90594a4511d1516155de225e
LJFrias <friaslukej@gmail.com>
2023-02-14 09:50:37 -0800

    intial upload

--- main.js
+++ main.js
@@ -30,27 +30,85 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
+const movePiece = (startStack, endStack) => {
+  // .pop() the last piece in the startStack .push() to the end of the endStack
+  let piece = stacks[startStack].pop() //might be stacks[startStack].pop()
+
+  stacks[endStack].push(piece)
 
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
+const isLegal = (startStack, endStack) => {
+  // I can move a startStack value to an empty endStack value= true
+    // is endStack is empty, stacks.endStack.length == 0
+  // startStack value to an endStack value with a higher number= true
+  // startStack value to an endStack value with a lower number = false
+    // slice(-1) to grab the last element of an array
+  // select a startStack that's empty = flase
+    // if startStack is empty, stacks.startStack.length == 0
+  // enter something besides a,b,c = false
+    // if !a or !b or !c
+  // this function should return true or flase
+
+if (((startStack === "a")||(startStack === "b")||(startStack === "c")) && ((endStack === "a")||(endStack === "b")||(endStack === "c"))){
+
+  if (stacks[startStack].length > 0) {
+
+    if ((stacks[endStack].length == 0) || (stacks[startStack].slice(-1) < stacks[endStack].slice(-1))){
+    return true
+    }
+
+    else {
+      return false
+    }
+
+  }
+
+else {
+  return false
+}
 
 }
 
+}
+
+
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
-  // Your code here
+  // if b array or c array = [4,3,2,1] you win
+  // assume isLegal caught all wrong moves, and the array is in the correct order
+  //so that means the game is over when b or c has 4 elements
+    // (stacks["b"].length = 4) || (stacks["c"].length = 4)
+  if ((stacks['b'].length == 4) || (stacks['c'].length == 4)) {
+    console.log("You win!")
+    return true
+  } 
+
+  else {
+    return false
+  }
 
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
+  // take in the two args
+  // check if isLegal
+    //if true, movePiece
+    //if false, display error message
+  // call checkForWin
+    //if true, display win message
+    //if false, keep playing
+  if (isLegal(startStack, endStack)) {
+    movePiece(startStack, endStack)
+    checkForWin()
+  }
 
+  else {
+    console.log("Illegal move, Try again")
+  }
+  
 }
 
 const getPrompt = () => {
